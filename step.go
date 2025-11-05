7c98131ec4b560534036cc57b35d320fbf2baaf3
chidi <chidi150c@gmail.com>
2025-11-05 16:00:09 -0600

    version 22 consolidateDUst refactored4

--- step.go
+++ step.go
@@ -1128,11 +1128,11 @@ func (t *Trader) step(ctx context.Context, c []Candle) (string, error) {
 	if isAdd && !skipPyramidGates {
 		dynamicGate := t.mirrorEffectiveGate(side) // ramped by nearest idx
 		if side == SideBuy {
-			if t.nearestIdxBuy >= 1 && t.nearestNetSell >= dynamicGate {
+			if t.nearestNetSell >= dynamicGate {
 				mirrorProfitOverride = true
 			}
 		} else {
-			if t.nearestIdxSell >= 1 && t.nearestNetBuy >= dynamicGate {
+			if t.nearestNetBuy >= dynamicGate {
 				mirrorProfitOverride = true
 			}
 		}
