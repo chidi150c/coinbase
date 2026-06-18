e1e7ff808a4375cbdc363f0155006a32fb6b6bdc
chidi <chidi150c@gmail.com>
2026-06-18 16:28:48 -0500

    feat(exit): add StopLoss: Stop Loss implemented ver-80

--- step.go
+++ step.go
@@ -1342,7 +1342,7 @@ func (t *Trader) step(ctx context.Context, execHistory []Candle, signalHistory [
 	totalLots := lsb + lss
 
 	log.Printf(
-		"[DEBUG] Total Lots=%d Raw=%s Decision=%s price=%.8f Reason=%s buyThresh=%.3f sellThresh=%.3f modelBuyThresh=%.3f modelSellThresh=%.3f LongOnly=%v ver-79",
+		"[DEBUG] Total Lots=%d Raw=%s Decision=%s price=%.8f Reason=%s buyThresh=%.3f sellThresh=%.3f modelBuyThresh=%.3f modelSellThresh=%.3f LongOnly=%v ver-80",
 		totalLots,
 		d.Raw,
 		d.Signal,
